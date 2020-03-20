521a7bf094951c2d5a4454863bbaeee5b43a2cee
rphi8796 <jlkfop@gmail.com>
2020-03-20 12:20:08 -0600

    Initial commit

---
+++ Assets/NewScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewScript : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "Y.. yy... yess.. very sorry sir... pll... pplll";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
